830da4f398abaa48fc9c7f234722265d1c017ac1
Anshraj <anshraj.05.dhakad@gmail.com>
2026-03-06 14:59:01 +0530

    fix visitor counter

--- api/visit.js
+++ api/visit.js
@@ -4,8 +4,12 @@ const redis = Redis.fromEnv();
 
 export default async function handler(req, res) {
 
-  const visits = await redis.incr("visits");
+  if (req.method === "POST") {
+    const visits = await redis.incr("visits");
+    return res.status(200).json({ visits });
+  }
 
+  const visits = await redis.get("visits") || 0;
   res.status(200).json({ visits });
 
 }
